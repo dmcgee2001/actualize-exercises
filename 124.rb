f8e2e3732ee549b77ea68e48c2c316497c76a374
Derek McGee <dmcgee2001@gmail.com>
2023-09-12 16:26:26 -0400

    Create 124.rb

---
+++ 124.rb
@@ -0,0 +1,20 @@
+puts "Enter a number"
+number = gets.chomp
+if number.to_i > 10
+    puts 1
+elsif number.to_i < 10
+    puts -1
+else
+    puts 0
+end
+
+
+
+x = 100
+if x < 5
+  puts 0
+elsif x < 50
+  puts 1
+else
+  puts 2
+end
