a29e3cdaecdda4056ceedaeddf0d52410acd9bb2
Derek McGee <dmcgee2001@gmail.com>
2023-09-12 15:37:17 -0400

    Create 121.rb

---
+++ 121.rb
@@ -0,0 +1,13 @@
+s = "foo"
+puts s.include? "f" #tells you is the defined string includes the other string, or if a word has a certain letter, etc.
+
+s = "Hello World!"
+s.swapcase!
+puts s #capitalizes lower case letters and lower cases capital letters
+
+s = "foo"
+puts s.clear #empties all characters in between the string quotes
+
+
+
+puts 3.14159265.swapcase! #trying to swap case on pi unsurprisingly doesnt swap the case
