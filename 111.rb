4241af4f277594dfd01dd2de1f542e18615f90cb
Derek McGee <dmcgee2001@gmail.com>
2023-09-12 14:38:49 -0400

    Create 111.rb

---
+++ 111.rb
@@ -0,0 +1,8 @@
+puts 2 + 2
+puts 15 * 2
+puts 15 - 3
+puts 1966 / 2
+
+puts 2 x 3
+#111.rb:6: syntax error, unexpected tIDENTIFIER, expecting end-of-input puts 2 x 3
+#there is an error on line 6 that ruby is unsure what I was attempting to tell the code
