c4aebdf0d235141b1e16e47be95ec9ced484004f
Derek McGee <dmcgee2001@gmail.com>
2023-09-14 19:14:17 -0400

    Create 142.rb

---
+++ 142.rb
@@ -0,0 +1,14 @@
+words = ["apple", "carrot", "banana"]
+index = 0
+while index < words.length
+    puts words[index]
+    index = index + 1
+end
+
+
+numbers = [1, 3, 5]
+index = 0
+while index < numbers.length
+    puts numbers[index].to_s * 3
+    index = index + 1
+end
