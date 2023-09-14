30ece1c85b24654cf1db47d5960199e4c44534bf
Derek McGee <dmcgee2001@gmail.com>
2023-09-14 19:32:45 -0400

    Create 144.rb

---
+++ 144.rb
@@ -0,0 +1,14 @@
+cars = [
+    { "make" => "honda", "model" => "civic", "color" => "red", "year" => "2018" },
+    { "make" => "ford", "model" => "mustang", "color" => "black", "year" => "2011" },
+    { "make" => "toyota", "model" => "camry", "color" => "blue", "year" => "2020" }
+]
+
+pp cars[0]["make"]
+
+students = [
+    { "name" => "Maria", "age" => 20, "major" => "Computer Science" },
+    { "name" => "Sofia", "age" => 21, "major" => "Mathematics" },
+    { "name" => "Jayden", "age" => 19, "major" => "Biology" }
+  ]
+  puts students[0]["age"]
