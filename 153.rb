1a25458209978926ca38e8e5d2244b68c2fe734f
Derek McGee <dmcgee2001@gmail.com>
2023-09-15 18:24:43 -0400

    Create 153.rb

---
+++ 153.rb
@@ -0,0 +1,25 @@
+cat1 = {"name" => "max", "breed" => "hairless", "age" => 15}
+
+pp cat1
+
+class Cat
+    def initialize(input_name, input_breed, input_age)
+        @name = input_name
+        @breed = input_breed
+        @age = input_age
+    end
+end
+
+cat = Cat.new("max", "hairless", 15)
+pp cat
+
+
+class StoreItem
+    def initialize(input_name, input_price)
+      @name = input_name
+      @price = input_price
+    end
+  end
+  
+  StoreItem = StoreItem.new("chair", 100)
+  pp StoreItem
