535e1acb36c834a0afa6cb2ef0de45f767eefcc3
Derek McGee <dmcgee2001@gmail.com>
2023-09-15 17:58:04 -0400

    Create 151.rb

---
+++ 151.rb
@@ -0,0 +1,25 @@
+def display_phrase
+puts "_______                                 __              ______                  __    __  __                 "
+puts "|       \                               |  \            |      \                |  \  |  \|  \               "
+puts "| $$$$$$$\  ______    ______    ______  | $$   __        \$$$$$$  _______       | $$  | $$ \$$ ______ ____   "
+puts "| $$  | $$ /      \  /      \  /      \ | $$  /  \        | $$   /       \      | $$__| $$|  \|      \    \  "
+puts "| $$  | $$|  $$$$$$\|  $$$$$$\|  $$$$$$\| $$_/  $$        | $$  |  $$$$$$$      | $$    $$| $$| $$$$$$\$$$$\ "
+puts "| $$  | $$| $$    $$| $$   \$$| $$    $$| $$   $$         | $$   \$$    \       | $$$$$$$$| $$| $$ | $$ | $$ "
+puts "| $$__/ $$| $$$$$$$$| $$      | $$$$$$$$| $$$$$$\        _| $$_  _\$$$$$$\      | $$  | $$| $$| $$ | $$ | $$ "
+puts "| $$    $$ \$$     \| $$       \$$     \| $$  \$$\      |   $$ \|       $$      | $$  | $$| $$| $$ | $$ | $$ "
+puts " \$$$$$$$   \$$$$$$$ \$$        \$$$$$$$ \$$   \$$       \$$$$$$ \$$$$$$$        \$$   \$$ \$$ \$$  \$$  \$$ "
+end                                                                                                 
+                                                                                                            
+display_phrase
+display_phrase
+display_phrase
+
+
+def print_lyrics
+    puts "Now this is a story all about how"
+    puts "My life got flipped turned upside down"
+    puts "And I'd like to take a minute, just sit right there"
+    puts "I'll tell you how I became the prince of a town called Bel-Air"
+end
+
+print_lyrics
