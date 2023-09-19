d292714e4e70d79e061ee356180b99869b695489
Derek McGee <dmcgee2001@gmail.com>
2023-09-19 15:05:53 -0400

    Create 204.rb

---
+++ 204.rb
@@ -0,0 +1,8 @@
+# QUESTION 1
+# Paste this link in your browser to make a copy of the Career Inventory Google document: 
+#     https://docs.google.com/document/d/1XaPaG6QXSQEajkHE1noa5pMfa27qmegSOY_FY6zBoQA/copy  
+# Fill in the answers for each question in the Career Inventory.
+# Change the sharing settings of the document to "Anyone with the link can view" 
+# Copy the link to your Career Inventory Google document and paste it below.
+
+https://docs.google.com/document/d/1h2Y8JgTr7Ldgjwltp_dKJYgY8JU-UsRdhozPnOnOt_0/edit?usp=sharing
