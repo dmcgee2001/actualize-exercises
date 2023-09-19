3eefd51f21f53b57c72670f9dd48e052c9fa6b86
Derek McGee <dmcgee2001@gmail.com>
2023-09-19 15:23:47 -0400

    Create 211.rb

---
+++ 211.rb
@@ -0,0 +1,11 @@
+# Write a method that takes in a number and returns the number times itself. Then run the method and print the result.
+def square_method(number)
+return number * number
+end
+
+
+square_method1 = square_method(8)
+
+pp square_method1
+
+# I was able to easily do this and feel I may have chosen one that was far too easy. tomorrow, I will likely try one of the intermediate ones or do something with classes to try to challenge myself a little more
