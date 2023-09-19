6d9238fd2f6bd24aa4f37c4fd0ae1afae23b6c6b
Derek McGee <dmcgee2001@gmail.com>
2023-09-19 16:20:13 -0400

    Add exercise solutions

---
+++ 222.rb
@@ -0,0 +1,16 @@
+puts "Enter your name."
+name = gets.chomp
+if name == "Martin"
+    puts "Hi, Martin!"
+else
+    puts "You're not Martin."
+end
+
+x = 1000
+if x < 5
+    puts 0
+elsif x < 10
+    puts 1
+else
+    puts 2
+end
