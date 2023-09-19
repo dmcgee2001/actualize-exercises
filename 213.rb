e63b9b67b7658828b03c549b3bfbb083b1644b32
Derek McGee <dmcgee2001@gmail.com>
2023-09-19 15:45:51 -0400

    Add exercise solutions

---
+++ 213.rb
@@ -0,0 +1,8 @@
+first_name = "Katherine"
+last_name = "Johnson"
+puts first_name + " " + last_name + " was a NASA research mathematician."
+puts "#{first_name} #{last_name} was a NASA research mathematician"
+
+name = "Salvador"
+age = 37
+puts name + " is " + age.to_s + " years old and goes by 'Chava' with his friends."
