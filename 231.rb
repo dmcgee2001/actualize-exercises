3b7c0d3be4a82d4f55d536a72285f3a36584584d
Derek McGee <dmcgee2001@gmail.com>
2023-09-21 19:29:33 -0400

    Add exercise solutions

---
+++ 231.rb
@@ -0,0 +1,9 @@
+# Write a program that asks the user to enter a name. If the name is not "Santa", print "You're not Santa."
+puts "What is your name?"
+name = gets.chomp 
+if name == "Santa"
+    puts
+else puts "You're not Santa" 
+end
+# i think the code that i wrote WORKS, however I wasn't familiar with the != code in the answer which im assuming means is not equal to
+# tomorrow i will try a different category to stay on my toes for all of them
