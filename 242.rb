94b4bd0366c52c3969b67b85a5e295b4ded7b70c
Derek McGee <dmcgee2001@gmail.com>
2023-09-21 20:23:21 -0400

    add exercise solutions

---
+++ 242.rb
@@ -0,0 +1,7 @@
+words = ['mother', 'father', 'son']
+words.push ('daughter')
+p words
+
+words = {'peanut' => 'legume crop grown mainly for its edible seeds', 'peach' => 'a deciduous tree first domesticated and cultivated in Zhejiang province of Eastern China'}
+words['dog'] = 'domesticated descendant of the wolf'
+p words
