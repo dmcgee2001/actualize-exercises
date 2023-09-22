2aa5348c83d77c3c34d0944471e590b891ba871f
Derek McGee <dmcgee2001@gmail.com>
2023-09-21 20:44:02 -0400

    add exercise solutions

---
+++ 243.rb
@@ -0,0 +1,8 @@
+product = {name: "Jacket", price: 49.99, color: "red"}
+puts product[:name] # the output is Jacket
+puts product[:price] # the output is 49.99
+
+
+fruits = { coconut: 1, apple: 2, banana: 3 }
+fruits[:pears] = 3
+pp fruits
