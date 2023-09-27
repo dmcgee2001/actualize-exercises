d3cd2497a51309c4cf431c7a19120a8ed7ed509f
Derek McGee <dmcgee2001@gmail.com>
2023-09-27 14:33:53 -0400

    add exercise solutions

---
+++ 323.rb
@@ -0,0 +1,8 @@
+# Create a program that multiplies by 2 until 100,000. 
+# That is, the program should print out: 1, 2, 4, 8, 16, 32, etc. until you get above 100,000.
+number = 1
+p number
+while number < 100000
+    number = number * 2
+    p number
+end
