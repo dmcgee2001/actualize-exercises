e06ade24c2a8bbdf9c78cc2936da83952d62779e
Derek McGee <dmcgee2001@gmail.com>
2023-09-27 17:54:00 -0400

    add exercise solutions

---
+++ 331.rb
@@ -0,0 +1,9 @@
+# Write a while loop that prints the even numbers from 2 to 40.
+index = 0
+number = 0
+while index < 40
+    number = number + 2
+    p number
+    index = index + 1 
+end
+# i definitely need more practice at loops, however more complex ones that really challenge me on my thinking and problem solving. tomorrow i will scroll through the deliberate practice and trry to find a challenging loop
