d0ecba5dee80df0bbe00e2c7f72bf7a49bfb8dc9
Derek McGee <dmcgee2001@gmail.com>
2023-09-27 14:18:50 -0400

    add exercise solutions

---
+++ 321.rb
@@ -0,0 +1,5 @@
+# Write a program that uses variables to store three different colors, then prints out a sentence using the colors with string concatenation (the + operator).
+color1 = "red"
+color2 = "yellow"
+color3 = "green"
+puts "I like the colors " + color1 + ", " + color2 + ", and " + color3 + "."
