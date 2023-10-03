3e087d90e7a6e5a7c68d8b16afe6f6d25d52ac63
Derek McGee <dmcgee2001@gmail.com>
2023-10-03 16:02:17 -0400

    add exercise solutions

---
+++ 414.rb
@@ -0,0 +1,9 @@
+# QUESTION 1
+# Read about the Ruby File class: https://rubyapi.org/3.2/o/s?q=file
+# Then write a program that creates a file called example.txt with the 
+# sentence "This is an example!" in it.
+
+text = "This is an example!"
+
+File.new('t.txt')
+File.write('t.txt', text)
