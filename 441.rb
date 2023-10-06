6d4d3c5fce58277ef4583c75961582f28a75ce64
Derek McGee <dmcgee2001@gmail.com>
2023-10-06 19:54:32 -0400

    add exercise solutions

---
+++ 441.rb
@@ -0,0 +1,15 @@
+# Write a Plant class with attributes for name, size, and price.
+
+class Plant
+    def initialize(name, size, price)
+      @name = name
+      @size = size
+      @price = price
+    end
+  end
+  
+  plant1 = Plant.new("Sunflower", "medium", 50)
+  p plant
+
+  #i think this is the last practice i will do with classes. they are very useful but i am geting used to them bery much and was able to do this with ease
+  
