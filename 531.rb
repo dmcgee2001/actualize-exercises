ccf2ebbe034d84412a7b1dcd31006337ec9c6805
Derek McGee <dmcgee2001@gmail.com>
2023-10-12 12:31:04 -0400

    add exercise solutions

---
+++ 531.rb
@@ -0,0 +1,10 @@
+# Write a program that uses variables to store the names of three cities, then prints out a sentence using that information with string concatenation (the + operator).
+
+
+city1 = "Tucson"
+city2 = "Gatlinburg"
+city3 = "New York City"
+
+puts city1 + ", " + city2 + ", and " + city3 + " are the 3 cities with the tallest people in the world."
+
+# I thought i would need more practice with something basic but this was easier than i thought
